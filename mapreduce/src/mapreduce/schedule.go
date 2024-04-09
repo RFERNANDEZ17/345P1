27d2eb3b69a759bbb18b175a62a1166a73b66228
RFERNANDEZ17 <141872209+RFERNANDEZ17@users.noreply.github.com>
2024-04-08 22:45:49 -0500

    2a update

--- mapreduce/src/mapreduce/schedule.go
+++ mapreduce/src/mapreduce/schedule.go
@@ -1,8 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-//
 // $chedule() starts and waits for all tasks in the given phase (mapPhase
 // or reducePhase). the mapFiles argument holds the names of the files that
 // are the inputs to the map phase, one per map task. nReduce is the
@@ -10,7 +12,6 @@ import "fmt"
 // of registered workers; each item is the worker's RPC address,
 // suitable for passing to call(). registerCh@n will yield all
 // existing registered workers (if any) and new ones as they register.
-//
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
@@ -30,5 +31,33 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part #2, 2B).
 	//
+	fmt.Printf("REGINA")
+	var wg sync.WaitGroup
+	fmt.Printf("wait group")
+	for i, file := range mapFiles {
+		// add 1 to the waitgroup for each task
+		wg.Add(1)
+		fmt.Printf("added ")
+		// Get worker address
+		address := <-registerChan
+		fmt.Println(address)
+		fmt.Printf("address")
+		// Make DoTaskArgs structs
+		taskArgs := DoTaskArgs{
+			JobName:    jobName,
+			File:       file,
+			Phase:      phase,
+			TaskNumber: i + 1}
+		fmt.Printf("taskargs ")
+		// Call the worker
+		go func() {
+			defer wg.Done()
+			call(address, "Worker.DoTask", taskArgs, nil)
+		}()
+		fmt.Printf("go call ")
+	}
+
 	fmt.Printf("Schedule: %v done\n", phase)
+	wg.Wait()
+	//return
 }
